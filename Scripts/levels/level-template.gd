b82a016d13fe9ecef3b4af16a9feedc5f95b6f67
Rick <rsulisthio@gmail.com>
2021-07-14 22:50:11 +0700

    added basic death mechanism

--- Scripts/levels/level-template.gd
+++ Scripts/levels/level-template.gd
@@ -1,26 +1,59 @@
 extends Node2D
 
+# level parameters
+export var target_time = 10.00
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var is_running = false												# level is running
+var current_time = 0.0
 
-onready var target_timer = get_node("bounce-timer")
 onready var target_timer_label = get_node("timer-text")
+onready var ball = get_node("ball")
+onready var reset_point = get_node("reset-point")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	target_timer.start()
+	ball.mode = RigidBody2D.MODE_STATIC
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	target_timer_label.text = str(target_timer.time_left)
+	if is_running:
+		current_time += delta
+		target_timer_label.text = str(current_time)
 
+func _on_playbutton_pressed():
+	is_running = true
+	ball.mode = RigidBody2D.MODE_RIGID
 
+func reset_gameplay():
+	is_running = false
+	target_timer_label.text = str(0)
+	
+	current_time = 0
+	
+	# respawn the ball
+	ball.global_position = reset_point.global_position
+	ball.mode = RigidBody2D.MODE_STATIC
 
-
-func _on_bouncetimer_timeout():
-	print("bitch")
+func _on_resetbutton_pressed():
+	reset_gameplay()
 	pass # Replace with function body.
+
+
+func _on_boundsleft_body_entered(body):
+	if body.name == "ball":
+		reset_gameplay()
+
+
+func _on_boundsright_body_entered(body):
+	if body.name == "ball":
+		reset_gameplay()
+
+func _on_boundstop_body_entered(body):
+	if body.name == "ball":
+		reset_gameplay()
+
+
+func _on_boundsbottom_body_entered(body):
+	if body.name == "ball":
+		reset_gameplay()
